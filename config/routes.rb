6ae235fef8b58a475811c8834f9fdc466a2ea314
Abby Cloft <acloft@uci.edu>
2018-05-22 23:26:01 -0700

    added surprise btn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
  
     match '/' => 'twitter#index', :via => :get
     match '/results' => 'twitter#results', :via => :get
+    root 'twitter#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
